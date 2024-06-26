57c802308c79c95afcff1705a30f78fb755526c5
EkaterinaMangileva <mangileva.ek@gmail.com>
2024-06-26 14:48:16 +0500

    Новый тест

--- tests/test_testers/conftest.py
+++ tests/test_testers/conftest.py
@@ -1,7 +1,7 @@
 import pytest
-from config import bot, chat_id
+from config import bot, daily
 
 
 @pytest.hookimpl(trylast=True)
 def pytest_sessionfinish(session, exitstatus):
-    bot.send_message(chat_id, "Пора на дэйли https://telemost.yandex.ru/j/28259899709322022272839333735427715996")
+    bot.send_message(daily, "Пора на дэйли https://telemost.yandex.ru/j/28259899709322022272839333735427715996")
